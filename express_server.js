045eebef27e0cbdae7816f6b57d6b21576cd6882
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-26 23:42:52 -0500

    Remove unsed code

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+
 const PORT =8080;
 
 const urlDatabase = {
@@ -9,26 +10,28 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  //res.json(urlDatabase);
+  res.render('index');
+});
+
+app.get("/urls", (req, res) => {
+  
+  const templateVars = { urls: urlDatabase };
+
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
