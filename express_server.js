ba878875db629454c91fd721ada7a5b67706a5ab
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-30 23:41:48 -0500

    Add redirect after successful generating tinyURL

--- express_server.js
+++ express_server.js
@@ -1,16 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-const PORT =8080;
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 8080;
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
-
+const generateRandomString = function (randomLength) {
+  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < randomLength; ++i) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,18 +27,24 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   //res.json(urlDatabase);
-  res.render('index');
+  res.render("index");
 });
 
 app.get("/urls", (req, res) => {
-  
   const templateVars = { urls: urlDatabase };
 
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]  };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -37,7 +52,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/urls", (req, res) => {
+  let tinyUrlId = generateRandomString(6);
+  urlDatabase[tinyUrlId] = req.body.longURL;
+res.redirect(`/urls/${tinyUrlId}`); // Respond with 'Ok' (we will replace this)
+});
 
+app.get("/u/:id", (req, res) => {
+  // const longURL = ...
+  res.redirect(longURL);
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
