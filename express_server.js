73923e7906597d1fb1ead99b29aaf2418e28ff38
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-06-08 17:12:12 -0500

    Implement automated test

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { url } = require("inspector");
 const bcrypt = require("bcryptjs");
-const { getUserByEmail } = require("./helper");
+const { getUserByEmail } = require("./helpers");
 //const cookieParser = require("cookie-parser");
 var cookieSession = require('cookie-session')
 const saltRounds = 10;
---
+++ test/helpersTest.js
@@ -0,0 +1,47 @@
+
+var chai = require('chai');  
+var assert = chai.assert; 
+
+const exp = require('constants');
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.equal(user.id,expectedUserID);
+ 
+  });
+
+  it('should return a valid user object', () => {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUser = {
+      
+      id: "userRandomID", 
+      email: "user@example.com", 
+      password: "purple-monkey-dinosaur"
+    
+  }
+  assert.deepEqual(user,expectedUser);
+  });
+
+  it('should return undefined for non-existence email', function() {
+    const user = getUserByEmail("user@example45.com", testUsers)
+    const expectedUserID = undefined;
+    assert.equal(user,expectedUserID);
+ 
+  });
+});
